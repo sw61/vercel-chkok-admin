4077190252654124e07be2ec012e73d58002dc44
sang1 <dign0614@gmail.com>
2025-07-15 16:47:20 +0900

    feat: PieChart 기능 추가

--- src/users/UserPieChart.tsx
+++ src/users/UserPieChart.tsx
@@ -22,43 +22,15 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const description = "A pie chart with a label";
-
-const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 90, fill: "var(--color-other)" },
-];
-
-const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  chrome: {
-    label: "Chrome",
-    color: "var(--chart-1)",
-  },
-  safari: {
-    label: "Safari",
-    color: "var(--chart-2)",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "var(--chart-3)",
-  },
-  edge: {
-    label: "Edge",
-    color: "var(--chart-4)",
-  },
-  other: {
-    label: "Other",
-    color: "var(--chart-5)",
-  },
-} satisfies ChartConfig;
+interface Status {
+  userCount: number;
+  clientCount: number;
+  activeUsers: number;
+  inactiveUsers: number;
+}
 
 export function UserPieChart() {
-  const [userStatus, setUserStatus] = useState();
+  const [userStatus, setUserStatus] = useState<Status | null>(null);
   const navigate = useNavigate();
   const getUserStatus = async () => {
     try {
@@ -75,27 +47,78 @@ export function UserPieChart() {
   useEffect(() => {
     getUserStatus();
   }, []);
+  const chartData = [
+    { status: "Total", userCount: userStatus?.userCount, fill: "#3B82F6" },
+    {
+      status: "Clients",
+      userCount: userStatus?.clientCount,
+      fill: "#10B981",
+    },
+    {
+      status: "Active",
+      userCount: userStatus?.activeUsers,
+      fill: "#F59E0B",
+    },
+    {
+      status: "Inactive",
+      userCount: userStatus?.inactiveUsers,
+      fill: "#EF4444",
+    },
+  ];
+  const chartConfig = {
+    value: {
+      label: "Value",
+    },
+    userCount: {
+      label: "UserCount",
+      color: "#3B82F6",
+    },
+    ClientUsers: {
+      label: "ClientUsers",
+      color: "#10B981",
+    },
+    activeUsers: {
+      label: "ActiveUsers",
+      color: "#F59E0B",
+    },
+    inactiveUsers: {
+      label: "InActiveUsers",
+      color: "#EF4444",
+    },
+  } satisfies ChartConfig;
+
   return (
-    <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Label</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="[&_.recharts-pie-label-text]:fill-foreground mx-auto aspect-square max-h-[250px] pb-0"
-        >
-          <PieChart>
-            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-            <Pie data={chartData} dataKey="visitors" label nameKey="browser" />
-            <ChartLegend
-              content={<ChartLegendContent nameKey="browser" />}
-              className="-translate-y-2 flex-wrap gap-2 *:basis-1/4 *:justify-center"
-            />
-          </PieChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+    <>
+      <Card className="flex flex-col">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Pie Chart - Label</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig}
+            className="[&_.recharts-pie-label-text]:fill-foreground mx-auto aspect-square max-h-[250px] pb-0"
+          >
+            <PieChart>
+              <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+              <Pie
+                data={chartData}
+                dataKey="userCount"
+                label
+                nameKey="status"
+              />
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+        {/* <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter> */}
+      </Card>
+    </>
   );
 }
