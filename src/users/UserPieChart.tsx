ca0a21b36b9c9a649ebd90662eec8af74b09e997
sang1 <dign0614@gmail.com>
2025-07-17 15:16:50 +0900

    feat: PieChart API 연동 완료

--- src/users/UserPieChart.tsx
+++ src/users/UserPieChart.tsx
@@ -91,8 +91,7 @@ export function UserPieChart() {
     <>
       <Card className="flex flex-col">
         <CardHeader className="items-center pb-0">
-          <CardTitle>Pie Chart - Label</CardTitle>
-          <CardDescription>January - June 2024</CardDescription>
+          <CardTitle>사용자 통계</CardTitle>
         </CardHeader>
         <CardContent className="flex-1 pb-0">
           <ChartContainer
@@ -110,14 +109,6 @@ export function UserPieChart() {
             </PieChart>
           </ChartContainer>
         </CardContent>
-        {/* <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 leading-none font-medium">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="text-muted-foreground leading-none">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter> */}
       </Card>
     </>
   );
