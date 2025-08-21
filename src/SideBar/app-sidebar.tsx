b69c5198ced691a51a9b2069ed7857241accb205
sang1 <dign0614@gmail.com>
2025-08-21 21:10:56 +0900

    chore: 주석처리

--- src/SideBar/app-sidebar.tsx
+++ src/SideBar/app-sidebar.tsx
@@ -59,16 +59,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         url: "/banners",
         icon: BookOpen,
       },
-      {
-        title: "서버 데이터",
-        url: "/server",
-        icon: Server,
-      },
-      {
-        title: "마크다운 에디터",
-        url: "/editor",
-        icon: Edit,
-      },
+      // {
+      //   title: "서버 데이터",
+      //   url: "/server",
+      //   icon: Server,
+      // },
+      // {
+      //   title: "마크다운 에디터",
+      //   url: "/editor",
+      //   icon: Edit,
+      // },
     ],
     navSecondary: [
       {
