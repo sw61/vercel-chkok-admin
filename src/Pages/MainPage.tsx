9e8392e2cc011fb66bd976b6668f60e5065c6fa6
sw61 <dign0614@gmail.com>
2025-09-11 16:08:07 +0000

    Merge branch 'main' into develop

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -5,8 +5,8 @@ import ServerDashBoard from '@/Server/ServerDashBoard';
 export default function MainPage() {
   return (
     <>
-      <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="mb-2 grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="flex flex-1 flex-col gap-6 p-4">
+        <div className="mb-2 grid auto-rows-min gap-6 md:grid-cols-2 lg:grid-cols-3">
           <div className="aspect-video rounded-xl">
             <UserPieChartActive />
           </div>
