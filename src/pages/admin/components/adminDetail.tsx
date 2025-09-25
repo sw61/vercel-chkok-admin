9448dd144ee7fb8350df8c5d4724354cbf519b4a
sw61 <dign0614@gmail.com>
2025-09-25 15:37:29 +0000

    fix: 이미지 파일 빌드 실패 오류 수정

--- src/pages/admin/components/adminDetail.tsx
+++ src/pages/admin/components/adminDetail.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import PulseLoader from 'react-spinners/PulseLoader';
-import usericon from '../../../image/usericon.png';
+import usericon from '../../../../image/usericon.png';
 import { useQuery } from '@tanstack/react-query';
 import { getAdminData } from '@/services/admin/adminApi';
 
