35cd03abcac891e53a601514bb46ae79e0c3937f
sang1 <dign0614@gmail.com>
2025-10-01 22:07:37 +0900

    refactor: mutation 오류 수정 및 Suspense 설정

--- src/pages/users/components/detail/userDetail.tsx
+++ src/pages/users/components/detail/userDetail.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from 'react';
+import { Suspense, useState, type ChangeEvent } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
@@ -10,12 +10,17 @@ import UserDetailSkeleton from '@/pages/users/components/detail/usersDetailSkele
 import { CustomBadge } from '@/hooks/useBadge';
 import usericon from '@/image/usericon.png';
 import ActivitiesPage from '../activities/table/activitiesPage';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
 import {
   deleteUser,
   getUserDetail,
   putMemoUpdate,
   putUserStatus,
+  userToClient,
 } from '@/services/users/detail/detailApi';
 import { useAlertDialog } from '@/components/alertDialog/useAlertDialog';
 
@@ -27,13 +32,13 @@ export default function UserDetail() {
   const queryClient = useQueryClient();
 
   // 사용자 데이터 요청
-  const { data: userData, isPending } = useQuery({
+  const { data: userData } = useSuspenseQuery({
     queryKey: ['userDetail', userId],
     queryFn: () => getUserDetail(userId!),
   });
   // 사용자 활성화 / 비활성화
   const { mutate: statusMutate } = useMutation({
-    onMutate: putUserStatus,
+    mutationFn: putUserStatus,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['userDetail', userId],
@@ -46,7 +51,7 @@ export default function UserDetail() {
   });
   // 사용자 삭제
   const { mutate: deleteMutate } = useMutation({
-    onMutate: deleteUser,
+    mutationFn: deleteUser,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['userDetail', userId],
@@ -60,7 +65,7 @@ export default function UserDetail() {
   });
   // 사용자 메모 업데이트
   const { mutate: memoUpdateMutate } = useMutation({
-    onMutate: putMemoUpdate,
+    mutationFn: putMemoUpdate,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['userDetail', userId, userMemo],
@@ -76,12 +81,11 @@ export default function UserDetail() {
   });
   // 권한 승급
   const { mutate: userToClientMutate } = useMutation({
-    onMutate: deleteUser,
+    mutationFn: userToClient,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['userDetail', userId],
       });
-      navigate('/users');
       toast.success('클라이언트로 승급되었습니다.');
     },
     onError: () => {
@@ -93,9 +97,7 @@ export default function UserDetail() {
   const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setUserMemo(event.target.value);
   };
-  if (isPending) {
-    return <UserDetailSkeleton />;
-  }
+
   if (!userData) {
     return <div>데이터가 없습니다.</div>;
   }
@@ -148,126 +150,134 @@ export default function UserDetail() {
   });
 
   return (
-    <div className="min-w-[650px] px-6 py-4">
-      <div className="grid grid-cols-1 gap-6">
-        <div>
-          <ChevronLeft
-            onClick={() => navigate('/users')}
-            className="cursor-pointer"
-          />
-        </div>
-        <Card>
-          <div className="flex gap-4 px-6">
-            <CardTitle className="ck-title">사용자 정보</CardTitle>
-            <CustomBadge variant={userData.role} />
+    <Suspense fallback={<UserDetailSkeleton />}>
+      <div className="min-w-[650px] px-6 py-4">
+        <div className="grid grid-cols-1 gap-6">
+          <div>
+            <ChevronLeft
+              onClick={() => navigate('/users')}
+              className="cursor-pointer"
+            />
           </div>
-          <CardContent className="pt-2">
-            <div className="flex justify-between pb-4">
-              <div className="flex items-center gap-4">
-                <Avatar className="h-16 w-16">
-                  <AvatarImage
-                    src={userData.profileImg || usericon}
-                    alt={userData.nickname}
-                  />
-                  <AvatarFallback></AvatarFallback>
-                </Avatar>
+          <Card>
+            <div className="flex gap-4 px-6">
+              <CardTitle className="ck-title">사용자 정보</CardTitle>
+              <CustomBadge variant={userData.role} />
+            </div>
+            <CardContent className="pt-2">
+              <div className="flex justify-between pb-4">
+                <div className="flex items-center gap-4">
+                  <Avatar className="h-16 w-16">
+                    <AvatarImage
+                      src={userData.profileImg || usericon}
+                      alt={userData.nickname}
+                    />
+                    <AvatarFallback></AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="ck-body-2">
+                      {userData.nickname} ({userData.gender || '성별 미공개'},{' '}
+                      {userData.age || '나이 미공개'})
+                    </p>
+                    <p className="ck-body-2 text-gray-500">
+                      {userData.email || '이메일 없음'}
+                    </p>
+                    <p className="ck-body-2 text-gray-500">
+                      {userData.phone || '전화번호 없음'}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-2 px-4">
+                  {userData.role === '사용자' && <UserToClientAlertDialog />}
+                  <StatusAlertDialog />
+                  <DeleteAlertDialog />
+                </div>
+              </div>
+
+              <div className="mb-6 grid grid-cols-3 gap-6">
                 <div>
+                  <p className="ck-caption-1 text-ck-gray-600">사용자 ID</p>
+                  <p className="ck-body-2">{userData.id}</p>
+                </div>
+
+                <div>
+                  <p className="ck-caption-1 text-ck-gray-600">계정 상태</p>
+                  <p className="ck-body-2 text-ck-blue-500">
+                    {userData.active ? '활성화' : '비활성화'}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">이메일 인증</p>
+                  <div className="ck-body-2">
+                    <p className="text-ck-blue-500 ck-body-2">
+                      {userData.emailVerified ? '인증됨' : '인증 필요'}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">계정 서비스</p>
+                  <p className="ck-body-2">{userData.provider}</p>
+                </div>
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">계정 타입</p>
+                  <p className="ck-body-2">{userData.accountType}</p>
+                </div>
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">계정 플랫폼</p>
                   <p className="ck-body-2">
-                    {userData.nickname} ({userData.gender || '성별 미공개'},{' '}
-                    {userData.age || '나이 미공개'})
-                  </p>
-                  <p className="ck-body-2 text-gray-500">
-                    {userData.email || '이메일 없음'}
-                  </p>
-                  <p className="ck-body-2 text-gray-500">
-                    {userData.phone || '전화번호 없음'}
+                    {userData.platforms ?? '정보 없음'}
                   </p>
                 </div>
-              </div>
-
-              <div className="flex gap-2 px-4">
-                {userData.role === '사용자' && <UserToClientAlertDialog />}
-                <StatusAlertDialog />
-                <DeleteAlertDialog />
-              </div>
-            </div>
-
-            <div className="mb-6 grid grid-cols-3 gap-6">
-              <div>
-                <p className="ck-caption-1 text-ck-gray-600">사용자 ID</p>
-                <p className="ck-body-2">{userData.id}</p>
-              </div>
-
-              <div>
-                <p className="ck-caption-1 text-ck-gray-600">계정 상태</p>
-                <p className="ck-body-2 text-ck-blue-500">
-                  {userData.active ? '활성화' : '비활성화'}
-                </p>
-              </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">이메일 인증</p>
-                <div className="ck-body-2">
-                  <p className="text-ck-blue-500 ck-body-2">
-                    {userData.emailVerified ? '인증됨' : '인증 필요'}
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">생성일</p>
+                  <p className="ck-body-2">
+                    {userData.createdAt.split('T')[0]}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-ck-gray-600 ck-caption-1">업데이트일</p>
+                  <p className="ck-body-2">
+                    {userData.updatedAt.split('T')[0]}
                   </p>
                 </div>
               </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">계정 서비스</p>
-                <p className="ck-body-2">{userData.provider}</p>
-              </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">계정 타입</p>
-                <p className="ck-body-2">{userData.accountType}</p>
-              </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">계정 플랫폼</p>
-                <p className="ck-body-2">{userData.platforms ?? '정보 없음'}</p>
-              </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">생성일</p>
-                <p className="ck-body-2">{userData.createdAt.split('T')[0]}</p>
-              </div>
-              <div>
-                <p className="text-ck-gray-600 ck-caption-1">업데이트일</p>
-                <p className="ck-body-2">{userData.updatedAt.split('T')[0]}</p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="ck-body-2">사용자 메모</p>
-              {hideMemo ? (
-                <div className="flex flex-col gap-4">
-                  <Textarea
-                    placeholder="텍스트를 입력하세요."
-                    value={userMemo}
-                    onChange={handleTextAreaChange}
-                  />
-                </div>
-              ) : (
-                <div className="ck-body-2 border-ck-gray-300 rounded-md border bg-transparent px-3 py-2">
-                  {userData.memo ? userData.memo : '내용이 없습니다.'}
-                </div>
-              )}
+              <div className="flex flex-col gap-2">
+                <p className="ck-body-2">사용자 메모</p>
+                {hideMemo ? (
+                  <div className="flex flex-col gap-4">
+                    <Textarea
+                      placeholder="텍스트를 입력하세요."
+                      value={userMemo}
+                      onChange={handleTextAreaChange}
+                    />
+                  </div>
+                ) : (
+                  <div className="ck-body-2 border-ck-gray-300 rounded-md border bg-transparent px-3 py-2">
+                    {userData.memo ? userData.memo : '내용이 없습니다.'}
+                  </div>
+                )}
 
-              {hideMemo ? (
-                <div className="flex justify-end">
-                  <UpdateAlertDialog />
-                </div>
-              ) : (
-                <div className="flex justify-end">
-                  <Button
-                    className="ck-body-1 text-ck-gray-900 hover:bg-ck-gray-300 cursor-pointer border bg-white"
-                    onClick={() => setHideMemo(true)}
-                  >
-                    수정
-                  </Button>
-                </div>
-              )}
-            </div>
-          </CardContent>
-        </Card>
-        <ActivitiesPage />
+                {hideMemo ? (
+                  <div className="flex justify-end">
+                    <UpdateAlertDialog />
+                  </div>
+                ) : (
+                  <div className="flex justify-end">
+                    <Button
+                      className="ck-body-1 text-ck-gray-900 hover:bg-ck-gray-300 cursor-pointer border bg-white"
+                      onClick={() => setHideMemo(true)}
+                    >
+                      수정
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+          <ActivitiesPage />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
