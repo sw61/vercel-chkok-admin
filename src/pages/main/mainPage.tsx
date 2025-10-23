5721cef89697dce6559362a7a5e9054172307cde
sw61 <dign0614@gmail.com>
2025-10-23 14:01:58 +0000

    feat: 그라파나 대시보드 페이지 추가

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -1,11 +1,11 @@
 import { UserPieChartActive } from '@/pages/users/pages/usersPieChartActive';
 import { UserPieChartCount } from '@/pages/users/pages/usersPieChartCount';
 import { CamapaignsPieChart } from '@/pages/campaigns/pages/campaignsPieChart';
-import ServerDashBoard from '@/pages/dashboard/components/serverDashBoard';
+
 export default function MainPage() {
   return (
     <>
-      <div className="flex flex-1 flex-col gap-2 ">
+      <div className="flex flex-1 flex-col gap-2">
         <div className="mb-2 grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-3">
           <div className="aspect-video rounded-xl">
             <UserPieChartActive />
@@ -17,9 +17,6 @@ export default function MainPage() {
             <CamapaignsPieChart />
           </div>
         </div>
-        {/* <div className="rounded-xl">
-          <ServerDashBoard />
-        </div> */}
       </div>
     </>
   );
