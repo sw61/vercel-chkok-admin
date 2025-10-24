1cba19c051b5cd9f7cddd86d11ac3d359f1631db
sw61 <dign0614@gmail.com>
2025-10-24 15:24:56 +0000

    fix: 그라파나 대시보드 크기 수정

--- src/pages/grafana/mainGrafanaDashboard.tsx
+++ src/pages/grafana/mainGrafanaDashboard.tsx
@@ -1,10 +1,10 @@
 import { Card } from '@/components/ui/card';
 
-export default function GrafanaDashboard() {
+export default function MainGrafanaDashboard() {
   return (
     <Card className="overflow-y-hidden p-0">
       <iframe
-        src="http://jenkins.chkok.kr:3000/d/rYdddlPWk/node-exporter-full?orgId=1&kiosk=tv&theme=light"
+        src="http://jenkins.chkok.kr:3000/d/linux-stats/1-linux-stats-with-node-exporter?orgId=1&kiosk=tv&theme=light"
         width="100%"
         height="800px"
         frameBorder="0"
