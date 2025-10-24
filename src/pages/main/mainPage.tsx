27ebf521ab675db8b2cd46e4e6080509599b52dc
sang1 <dign0614@gmail.com>
2025-10-25 00:24:24 +0900

    fix: 그라파나 대시보드 크기 수정

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -1,23 +1,5 @@
-import { UserPieChartActive } from '@/pages/users/pages/usersPieChartActive';
-import { UserPieChartCount } from '@/pages/users/pages/usersPieChartCount';
-import { CamapaignsPieChart } from '@/pages/campaigns/pages/campaignsPieChart';
+import MainGrafanaDashboard from '../grafana/mainGrafanaDashboard';
 
 export default function MainPage() {
-  return (
-    <>
-      <div className="flex flex-1 flex-col gap-2">
-        <div className="mb-2 grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <div className="aspect-video rounded-xl">
-            <UserPieChartActive />
-          </div>
-          <div className="aspect-video rounded-xl">
-            <UserPieChartCount />
-          </div>
-          <div className="aspect-video rounded-xl">
-            <CamapaignsPieChart />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <MainGrafanaDashboard />;
 }
