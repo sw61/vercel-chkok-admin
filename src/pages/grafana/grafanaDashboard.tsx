a60c0b4ed18a28340a7de5fae69706e0fc451ef7
sw61 <dign0614@gmail.com>
2025-10-29 03:08:40 +0000

    fix: https 그라파나 주소변경

--- src/pages/grafana/grafanaDashboard.tsx
+++ src/pages/grafana/grafanaDashboard.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/components/ui/card';
 
 export default function GrafanaDashboard() {
-  const grafanaUrl = `http://grafana.chkok.kr/grafana/d/rYdddlPWk/node-exporter-full?orgId=1&kiosk=tv&theme=light`;
+  const grafanaUrl = `https://grafana.chkok.kr/grafana/d/rYdddlPWk/node-exporter-full?orgId=1&kiosk=tv&theme=light`;
 
   return (
     <Card className="overflow-y-hidden p-0">
--- src/pages/grafana/mainGrafanaDashboard.tsx
+++ src/pages/grafana/mainGrafanaDashboard.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/components/ui/card';
 
 export default function MainGrafanaDashboard() {
-  const grafanaUrl = `http://grafana.chkok.kr/grafana/d/linux-stats/1-linux-stats-with-node-exporter?orgId=1&kiosk=tv&theme=light`;
+  const grafanaUrl = `https://grafana.chkok.kr/grafana/d/linux-stats/1-linux-stats-with-node-exporter?orgId=1&kiosk=tv&theme=light`;
 
   return (
     <Card className="overflow-y-hidden p-0">
