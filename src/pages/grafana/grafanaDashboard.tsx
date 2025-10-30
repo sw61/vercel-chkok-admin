beaae21cf8b9c7bf87db843cb4ff4bcc1566dceb
sang1 <dign0614@gmail.com>
2025-10-31 00:55:14 +0900

    fix: 프록시 헤더 제거

--- src/pages/grafana/grafanaDashboard.tsx
+++ src/pages/grafana/grafanaDashboard.tsx
@@ -2,14 +2,14 @@ import { Card } from '@/components/ui/card';
 
 export default function GrafanaDashboard() {
   const grafanaPath =
-    '/public-dashboards/5b8e97ac2c4141788e8004d18179923b?orgId=1&kiosk=tv&theme=light';
+    'https://grafana.chkok.kr/public-dashboards/5b8e97ac2c4141788e8004d18179923b?orgId=1&kiosk=tv&theme=light';
 
-  const proxyUrl = `/api/grafana-proxy?path=${encodeURIComponent(grafanaPath)}`;
+  // const proxyUrl = `/api/grafana-proxy?path=${encodeURIComponent(grafanaPath)}`;
 
   return (
     <Card className="overflow-y-hidden p-0">
       <iframe
-        src={proxyUrl}
+        src={grafanaPath}
         width="100%"
         height="800px"
         frameBorder="0"
--- src/pages/grafana/mainGrafanaDashboard.tsx
+++ src/pages/grafana/mainGrafanaDashboard.tsx
@@ -1,14 +1,14 @@
 import { Card } from '@/components/ui/card';
 
 export default function MainGrafanaDashboard() {
-  const grafanaPath = `/public-dashboards/c194255582cf42d0ac712b5df9c47778?orgId=1&kiosk=tv&theme=light`;
+  const grafanaPath = `https://grafana.chkok.kr/public-dashboards/c194255582cf42d0ac712b5df9c47778?orgId=1&kiosk=tv&theme=light`;
 
-  const proxyUrl = `/api/grafana-proxy?path=${encodeURIComponent(grafanaPath)}`;
+  // const proxyUrl = `/api/grafana-proxy?path=${encodeURIComponent(grafanaPath)}`;
 
   return (
     <Card className="overflow-y-hidden p-0">
       <iframe
-        src={proxyUrl}
+        src={grafanaPath}
         width="100%"
         height="800px"
         frameBorder="0"
